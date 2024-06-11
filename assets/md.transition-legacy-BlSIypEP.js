b0408300dd41eada23e9ddd97c31d3015f4a8e3d
Мичил Хабаров <khabarovmichil@gmail.com>
2024-06-11 22:58:38 +0200

    deploy

--- assets/md.transition-legacy-BlSIypEP.js
+++ assets/md.transition-legacy-BlSIypEP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DIx108x2.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-D8d5vFAO.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
