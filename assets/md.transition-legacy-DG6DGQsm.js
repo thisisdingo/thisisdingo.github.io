a95726eac3b98c06b8a1d8b06372240a221967c9
Мичил Хабаров <khabarovmichil@gmail.com>
2024-06-11 23:11:30 +0200

    deploy

--- assets/md.transition-legacy-DG6DGQsm.js
+++ assets/md.transition-legacy-DG6DGQsm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BmDFYz8m.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-CnAUBtiV.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
