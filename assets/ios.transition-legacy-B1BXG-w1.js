8387008d1be4064bd6407634300746d20f8892ad
Мичил Хабаров <khabarovmichil@gmail.com>
2024-06-11 23:13:08 +0200

    deploy

--- assets/ios.transition-legacy-B1BXG-w1.js
+++ assets/ios.transition-legacy-B1BXG-w1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CnAUBtiV.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BtUCfJ8H.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-B6fATcOr.js
+++ assets/keyboard2-legacy-B6fATcOr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CnAUBtiV.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BtUCfJ8H.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
